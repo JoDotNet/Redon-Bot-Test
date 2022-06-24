6efd7a315d1167d218ad5b93bb7595df0cfceae3
XxJOxX25 <johnoscarbhv@gmail.com>
2022-06-24 21:34:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import nextcord
 from nextcord.ext import commands
+from nextcord import Interaction
+
 
 import os
 
@@ -10,5 +12,10 @@ async def on_start():
     print(f'Logged in as: {bot.user}')
 
 
+@bot.slash_command(name = "help", description = "help me")
+async def help(ctx: Interaction):
+    await ctx.send("Hello!")
+
+    
     
 bot.run(os.environ['BOT_TOKEN'])
