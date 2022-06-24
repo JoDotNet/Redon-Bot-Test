3e7f77d26c128bf0d98cc4999e5c4567a6b90295
XxJOxX25 <johnoscarbhv@gmail.com>
2022-06-24 21:59:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ async def on_start():
     print(f'Logged in as: {bot.user}')
 
 
-@bot.slash_command(name = "help", description = "help me")
+@bot.slash_command(name = "help", description = "help me", guild_ids=[982973690478211072])
 async def help(ctx: Interaction):
     await ctx.send("Hello!")
 
     
-    
+
 bot.run(os.environ['BOT_TOKEN'])
